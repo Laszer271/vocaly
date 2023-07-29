d0b7c280d1f1ab042e14d31d3807a39ea4160f51
bartoszkozakiewicz <bartosz040824@gmail.com>
2023-07-29 17:22:31 +0200

    Remove Handling, Audio Handling, etc...

--- frontend/src/components/MediaComponent.js
+++ frontend/src/components/MediaComponent.js
@@ -10,13 +10,17 @@ const MediaComponent = ({ type }) => {
     dinosaurs and extraordinary creatures roamed
     the lands, seas, and skies.`;
   const [selectedFile, setSelectedFile] = useState();
+  const [audioFileUrl, setAudioFileUrl] = useState();
+  const [videoFileUrl, setVideoFileUrl] = useState();
+  const [playing, setPlaying] = useState(false);
+
   // handle file upload
   const handleUpload = async () => {
     const formData = new FormData();
     formData.append("file", selectedFile);
 
     // replace with your server URL
-    const url = "https://your-server-url/path-to-upload-endpoint";
+    const url = "/video";
 
     await axios.post(url, formData, {
       headers: {
@@ -25,53 +29,84 @@ const MediaComponent = ({ type }) => {
     });
 
     // if upload successful, set file URL to local object URL
-    setFileUrl(URL.createObjectURL(selectedFile));
+    title==="Video" ? setVideoFileUrl(URL.createObjectURL(selectedFile)) : setAudioFileUrl(URL.createObjectURL(selectedFile))
   };
-
-  const [fileUrl, setFileUrl] = useState();
-  const [playing, setPlaying] = useState(false);
 
   // handle file selection
-  const handleFileChange = (e) => {
-    setFileUrl(URL.createObjectURL(e.target.files[0]));
+  const handleFileAdd = (e) => {
+    console.log("ADD")
+    title==="Video" ? setVideoFileUrl(URL.createObjectURL(e.target.files[0])) : setAudioFileUrl(URL.createObjectURL(e.target.files[0]))
     setPlaying(false);
+
+    
   };
 
-  // handle play control
+  const handleFileRemove = () =>{
+    console.log("DELETE")
+    title==="Video" ? setVideoFileUrl(null) : setAudioFileUrl(null)
+    setPlaying(false);
+  }
+
+    // handle play control
   const handlePlayChange = () => {
-    setPlaying(!playing);
-  };
+      setPlaying(!playing);
+    };
+  
 
-  // handle pause control
-  const handlePause = () => {
-    setPlaying(false);
-  };
   return (
     <div className="media-component">
-      <div className="flex flex-row justify-between p-10 ">
-        <div className="w-1/2 text-left">
-          <h1 className="font-bold text-4xl mb-2">{title}</h1>
-          <p className="text-lg mb-2">{description}</p>
-          <p className="text-sm text-gray-500">Be patient!</p>
-        </div>
-        <div className="w-1/2 flex flex-col items-center justify-center space-y-3">
-          <InputFile icon={true} value={"Place your " + title} onFileChange={handleFileChange}/>
-          <Button value={"Remove"} />
-        </div>
-      </div>
-      <div className="flex flex-row justify-between">
-        <div className="w-1/2 flex flex-col items-center justify-center space-y-3">
-          {fileUrl && (
-            <>
-              <ReactPlayer width="100%" height="70%" playing={playing} url={fileUrl} />
-              <Button ifIcon={true} handleClick={handlePlayChange} />
-            </>
-          )}
-        </div>
-        <div className="w-1/2 flex flex-col items-center justify-center space-y-3">
-  
-        </div>
-      </div>
+      {type === 'Video' ? (
+        <>
+          <div className="flex flex-row justify-between p-10">
+            <div className="w-1/2 text-left">
+              <h1 className="font-bold text-4xl mb-2">{title}</h1>
+              <p className="text-lg mb-2">{description}</p>
+              <p className="text-sm text-gray-500">Be patient!</p>
+            </div>
+            <div className="w-1/2 flex flex-col items-center justify-center space-y-3">
+              <InputFile icon={true} value={"Place your " + title} onFileChange={handleFileAdd} />
+              <Button value={"Remove"} handleClick={handleFileRemove} />
+            </div>
+          </div>
+          <div className="flex flex-row justify-between">
+            <div className="w-1/2 flex flex-col items-center justify-center space-y-3">
+              {videoFileUrl && (
+                <>
+                  <ReactPlayer width="100%" height="70%" playing={playing} url={videoFileUrl} />
+                  <Button ifIcon={true} handleClick={handlePlayChange} />
+                </>
+              )}
+            </div>
+            <div className="w-1/2 flex flex-col items-center justify-center space-y-3"></div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="flex flex-row justify-between p-10">
+            <div className="w-1/2 text-left">
+              <h1 className="font-bold text-4xl mb-2">{title}</h1>
+              <p className="text-lg mb-2">{description}</p>
+              <p className="text-sm text-gray-500">Be patient!</p>
+            </div>
+            <div className="w-1/2 flex flex-col items-center justify-center space-y-3">
+              <InputFile icon={true} value={"Place your " + title} onFileChange={handleFileAdd} />
+              <Button value={"Remove"} handleClick={handleFileRemove} />
+            </div>
+          </div>
+          <div className="flex flex-row justify-between">
+            <div className="w-1/2 flex flex-col items-center justify-center space-y-3">
+              {audioFileUrl && (
+                <>
+                  <audio controls>
+                    <source src={audioFileUrl} type="audio/mpeg" />
+                  </audio>
+                </>
+              )}
+            </div>
+            <div className="w-1/2 flex flex-col items-center justify-center space-y-3"></div>
+          </div>
+        </>
+      )}
     </div>
   );
   
