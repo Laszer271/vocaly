1c8117fa5ce98bd4f699a4bbfa27109eb06980d4
bartoszkozakiewicz <bartosz040824@gmail.com>
2023-07-30 16:26:34 +0200

    Text addition components in frontend

--- frontend/src/components/MediaComponent.js
+++ frontend/src/components/MediaComponent.js
@@ -4,7 +4,8 @@ import Button from "./button";
 import InputFile from "./inputFile"
 import {Puff} from "react-loader-spinner"
 import Settings from "./settings";
-import { url } from "../utilis/url";
+import TextEditor from "./textEditor";
+
 
 const MediaComponent = ({ type }) => {
   const title = type;
@@ -24,6 +25,8 @@ const MediaComponent = ({ type }) => {
   const [returnedAudio,setReturnedAudio] = useState(null)
   const [videoLoading, setVideoLoading] = useState(false)
   const [audioLoading, setAudioLoading] = useState(false)
+  const [editText,setEditText]=useState('')
+  const [doTextEdit,setDoTextEdit]=useState(false) 
 
   // handle file selection
   const handleFileAdd = async (e) => {
@@ -134,6 +137,7 @@ const MediaComponent = ({ type }) => {
     } 
   };
 
+  //Handle file removing
   const handleFileRemove = () =>{
     console.log("DELETE")
     if (title === "Video") {
@@ -147,6 +151,11 @@ const MediaComponent = ({ type }) => {
     setPlayingProcessed(false)
   }
 
+  //Handle text edition
+  function handleTextEdition(){
+    setDoTextEdit(true)
+  }
+
     // handle play control
   const handlePlayChangeOriginal = () => {
     setPlayingOriginal(prevPlayingOriginal => !prevPlayingOriginal);
@@ -158,40 +167,49 @@ const MediaComponent = ({ type }) => {
   
 
   return (
-    <div className="media-component">
-      {type === 'Video' ? (
-        <>
-          <div className="flex flex-row justify-between p-6 mb-0">
-            <div className="w-1/3 text-left">
-              <h1 className="font-bold text-4xl mb-2">{'VoiceCleaner'}</h1>
-              <p className="text-lg mb-2">{'Tourettes Talker is an innovative app designed to help individuals with Tourettes Syndrome or stuttering communicate more confidently in English. With user-friendly features, it empowers users to express themselves with ease and clarity.'}</p>
-              <p className="text-sm text-gray-500">Be patient!</p>
-            </div>
-            <div className="w-1/3 flex flex-col items-center justify-center mt-0">
-              {error && error}
-              <InputFile icon={true} value={"Place your " + title} onFileChange={handleFileAdd} />
-              <Button value={"Remove"} handleClick={handleFileRemove} />
-            </div>
-
-            <div className="w-1/3 flex flex-col items-center justify-center">
-                <Settings setFormData={setFormData} formData={formData} setVoiceSample={setVoiceSample}/>
-            </div>
-
-              {/* <div className="mt-6">
-              <Button value={"Settings"} handleClick={handleFileRemove} />
-              </div> */}
+      <div className="media-component">
+    {type === 'Video' ? (
+      <>
+        <div className="flex flex-row justify-between p-6 mb-0">
+          <div className="w-1/3 text-left">
+            <h1 className="font-bold text-4xl mb-2">{'VoiceCleaner'}</h1>
+            <p className="text-lg mb-2">{'Tourettes Talker is an innovative app designed to help individuals with Tourettes Syndrome or stuttering communicate more confidently in English. With user-friendly features, it empowers users to express themselves with ease and clarity.'}</p>
+            <p className="text-sm text-gray-500">Be patient!</p>
           </div>
-          <div className="flex flex-row justify-between ">
-            <div className="w-1/2 flex flex-col items-center justify-center space-y-3">
-              {videoFileUrl && (
-                <>
-                  <ReactPlayer width="100%" height="60%" playing={playingOriginal} url={videoFileUrl} />
-                  <Button ifIcon={true} handleClick={handlePlayChangeOriginal} />
-                </>
-              )}
-            </div>
-            <div className="w-1/2 flex flex-col items-center justify-center space-y-3">
-            {returnedVideo ? (
+          <div className="w-1/3 flex flex-col items-center justify-center mt-0">
+            {error && error}
+            <InputFile icon={true} value={"Place your " + title} onFileChange={handleFileAdd} />
+            <Button value={"Remove"} handleClick={handleFileRemove} />
+            {returnedVideo && !doTextEdit ? (
+              <button onClick={handleTextEdition} className="flex w-64 h-10 items-center justify-center px-4 py-2 rounded-md backdrop-filter backdrop-blur-lg bg-white bg-opacity-20 mb-6">
+                Change Text
+              </button>
+            ) : (
+              <button onClick={handleTextEdition} className="flex w-64 h-10 items-center justify-center px-4 py-2 rounded-md backdrop-filter backdrop-blur-lg bg-white bg-opacity-20 mb-6">
+                Save Changes
+              </button>
+            )}
+          </div>
+          <div className="w-1/3 flex flex-col items-center justify-center">
+            <Settings setFormData={setFormData} formData={formData} setVoiceSample={setVoiceSample} />
+          </div>
+        </div>
+
+        {doTextEdit ? (
+            <TextEditor editText={editText} setEditText={setEditText} />
+          ) : null}
+        <div className="flex flex-row justify-between">
+
+          <div className="w-1/2 flex flex-col items-center justify-center space-y-3">
+            {(videoFileUrl && !doTextEdit) && (
+              <>
+                <ReactPlayer width="100%" height="60%" playing={playingOriginal} url={videoFileUrl} />
+                <Button ifIcon={true} handleClick={handlePlayChangeOriginal} />
+              </>
+            )}
+          </div>
+          <div className="w-1/2 flex flex-col items-center justify-center space-y-3">
+            {(returnedVideo && !doTextEdit) ? (
               <>
                 <ReactPlayer width="100%" height="60%" playing={playingProcessed} url={returnedVideo} />
                 <Button ifIcon={true} handleClick={handlePlayChangeProcessed} />
@@ -201,59 +219,61 @@ const MediaComponent = ({ type }) => {
                 <div className="text-2xl">Video preprocessing...</div>
                 <Puff color="#008080" height={100} width={100} />
               </>
-            ) : (
-              ""
-            )}
-            </div>
+            ) : null}
           </div>
-        </>
-      ) : (
-        <>
+        </div>
+      </>
+    ) : (
+      <>
         <div className="flex flex-row justify-between p-6 mb-0">
-            <div className="w-1/3 text-left">
-              <h1 className="font-bold text-4xl mb-2">{'VoiceCleaner'}</h1>
-              <p className="text-lg mb-2">{'Tourettes Talker is an innovative app designed to help individuals with Tourettes Syndrome or stuttering communicate more confidently in English. With user-friendly features, it empowers users to express themselves with ease and clarity.'}</p>
-              <p className="text-sm text-gray-500">Be patient!</p>
-            </div>
-            <div className="w-1/3 flex flex-col items-center justify-center mt-0">
-              {error && error}
-              <InputFile icon={true} value={"Place your " + title} onFileChange={handleFileAdd} />
-              <Button value={"Remove"} handleClick={handleFileRemove} />
-            </div>
-            <div className="w-1/3 flex flex-col items-center justify-center">
-                <Settings setFormData={setFormData} formData={formData} setVoiceSample={setVoiceSample}/>
-              </div>
+          <div className="w-1/3 text-left">
+            <h1 className="font-bold text-4xl mb-2">{'VoiceCleaner'}</h1>
+            <p className="text-lg mb-2">{'Tourettes Talker is an innovative app designed to help individuals with Tourettes Syndrome or stuttering communicate more confidently in English. With user-friendly features, it empowers users to express themselves with ease and clarity.'}</p>
+            <p className="text-sm text-gray-500">Be patient!</p>
           </div>
-          <div className="flex flex-row justify-between">
-            <div className="w-1/2 flex flex-col items-center justify-center mt-20">
-              {audioFileUrl && (
-                <>
-                  <audio controls>
-                    <source src={audioFileUrl} type="audio/mpeg" />
-                  </audio>
-                </>
-              )}
-            </div>
-            <div className="w-1/2 flex flex-col items-center justify-center mt-20">
+          <div className="w-1/3 flex flex-col items-center justify-center mt-0">
+            {error && error}
+            <InputFile icon={true} value={"Place your " + title} onFileChange={handleFileAdd} />
+            <Button value={"Remove"} handleClick={handleFileRemove} />
+            {returnedAudio && (
+              <button className="flex w-64 h-10 items-center justify-center px-4 py-2 rounded-md backdrop-filter backdrop-blur-lg bg-white bg-opacity-20 mb-6">
+                Change Text
+              </button>
+            )}
+          </div>
+          <div className="w-1/3 flex flex-col items-center justify-center">
+            <Settings setFormData={setFormData} formData={formData} setVoiceSample={setVoiceSample} />
+          </div>
+        </div>
+        <div className="flex flex-row justify-between">
+          <div className="w-1/2 flex flex-col items-center justify-center mt-20">
+            {audioFileUrl && (
+              <>
+                <audio controls>
+                  <source src={audioFileUrl} type="audio/mpeg" />
+                </audio>
+              </>
+            )}
+          </div>
+          <div className="w-1/2 flex flex-col items-center justify-center mt-20">
             {returnedAudio ? (
               <>
-                  <audio controls>
-                    <source src={returnedAudio} type="audio/mpeg" />
-                  </audio>  
+                <audio controls>
+                  <source src={returnedAudio} type="audio/mpeg" />
+                </audio>
               </>
             ) : audioLoading ? (
               <>
                 <div className="text-2xl">Audio preprocessing...</div>
                 <Puff color="#008080" height={100} width={100} />
               </>
-            ) : (
-              ""
-            )}
-            </div>
+            ) : null}
           </div>
-        </>
-      )}
-    </div>
+        </div>
+      </>
+    )}
+  </div>
+
   );
   
 };
