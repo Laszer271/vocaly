0586a2bb0ecc05b62685f326176d269f723ee6e3
bartoszkozakiewicz <bartosz040824@gmail.com>
2023-07-30 21:10:26 +0200

    url

--- frontend/src/utilis/url.js
+++ frontend/src/utilis/url.js
@@ -1,4 +1,4 @@
 // export const url = process.env.REACT_APP_API_URL || "http://localhost:8000";
 // export const url = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
-// export const url = "http://127.0.0.1:8000";
-export const url = "https://www.hback.readiy.tech";
+export const url = "http://127.0.0.1:8000";
+// export const url = "https://www.hback.readiy.tech";
